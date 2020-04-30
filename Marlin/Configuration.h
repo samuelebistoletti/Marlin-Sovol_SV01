5d4d10dfe341693ca98f467e002a4ee9952fc501
Samuele Bistoletti <samuele.bistoletti@gmail.com>
2020-04-30 15:06:51 +0200

    PID for e3d nozzle

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -483,25 +483,15 @@
 
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
-  // Ultimaker
-  //#define DEFAULT_Kp 22.2
-  //#define DEFAULT_Ki 1.08
-  //#define DEFAULT_Kd 114
+  // SOVOL SV01 with standard nozzle
+  // #define DEFAULT_Kp 27.58
+  // #define DEFAULT_Ki 2.49
+  // #define DEFAULT_Kd 76.24
 
-  // MakerGear
-  //#define DEFAULT_Kp 7.0
-  //#define DEFAULT_Ki 0.1
-  //#define DEFAULT_Kd 12
-
-  // Mendel Parts V9 on 12V
-  //#define DEFAULT_Kp 63.0
-  //#define DEFAULT_Ki 2.25
-  //#define DEFAULT_Kd 440
-
-  // SOVOL SV01 with stock CR-10S Hotend and fan 100%
-  #define DEFAULT_Kp 27.58
-  #define DEFAULT_Ki 2.49
-  #define DEFAULT_Kd 76.24
+  // SOVOL SV01 with E3D Nozzle
+  #define DEFAULT_Kp 27.87
+  #define DEFAULT_Ki 2.50
+  #define DEFAULT_Kd 77.63
 
 #endif // PIDTEMP
 
